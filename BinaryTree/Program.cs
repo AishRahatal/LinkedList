8b36edb0c7ebaa7524deb134c5963ca5e4dbefd0
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-10-03 19:45:14 +0530

    added

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static BinaryTree.Tree;
+
+namespace BinaryTree
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Binary Tree :");
+            Console.WriteLine("--------------------------");
+
+            Tree t = new Tree();
+            t.insert( 56);
+            t.insert(30);
+            t.insert(70);
+            t.insert(22);
+            t.insert(40);
+            t.insert(60);
+            t.insert(95);
+            t.insert(11);
+            t.insert(65);
+            t.insert(3);
+            t.insert(16);
+            t.insert(63);
+            t.insert(67);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Finding node 63: \n");
+
+           t.SearchNode(63);
+            
+            Console.WriteLine("----------------------------");
+
+         
+
+
+            
+
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace BinaryTree
+{
+
+    class Tree
+    {
+
+        public class Node
+        {
+            public Node Left;
+            public int data;
+
+            public Node Right;
+
+        }
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public void insert(int data)
+        {
+            Node newnode = new Node();
+            newnode.data = data;
+
+            if (root == null)
+            {
+                root = newnode;
+                root.data = newnode.data;
+                Console.WriteLine(" Inserted to root :" + root.data);
+            }
+            else
+            {
+                Node parent = root;
+                if (parent.data > newnode.data)
+                {
+                    root.Left = newnode;
+                    Console.WriteLine(" Inserted to left :" + parent.Left.data);
+
+                }
+                else
+                {
+                    if (parent.data < newnode.data)
+                    {
+                        root.Right = newnode;
+                        Console.WriteLine(" Inserted to right:" + parent.Right.data);
+                        Console.WriteLine("\n");
+
+                    }
+                }
+            }
+
+
+        }
+
+        
+
+        public void SearchNode(int key)
+        {
+
+            int depth = 0;
+            Node temp = new Node();
+            temp = root;
+
+            while (temp != null)
+            {
+                depth++;
+                if (temp.data == key)
+                {
+                    Console.WriteLine("\nData found at depth: " + depth);
+                    return;
+                }
+                else
+                {
+                    if (temp.data > key)
+                    {
+                        
+                        Console.WriteLine("\nData found at Left depth: " + depth);
+                        break;
+                        
+                    }
+                    else
+                    {
+                        temp = temp.Right;
+                        
+                        Console.WriteLine("\nData found at right depth: " + depth);
+                        break;
+                        
+                    }
+
+                }
+
+            }
+
+
+            return ;
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
